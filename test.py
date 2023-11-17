4dfd8abe3d8a5392fb8ac037f8e778d2168cdbbc
DESKTOP-79052US\Quoc deeptry <quocle300903@gmail.com>
2023-11-17 21:36:48 +0700

    Create project

---
+++ test.py
@@ -0,0 +1,13 @@
+while True:
+    sub_choice = int(input("Nhập lựa chọn của bạn (0 để quay lại): "))
+    if sub_choice == 0:
+        print("Quay lại chọn chương trình chính.")
+        break
+    elif sub_choice == 1:
+        print("Bạn đã chọn Option 1 trong Case 3.")
+    elif sub_choice == 2:
+        print("Bạn đã chọn Option 2 trong Case 3.")
+    elif sub_choice == 3:
+        print("Bạn đã chọn Option 3 trong Case 3.")
+    else:
+        print("Lựa chọn không hợp lệ.")
